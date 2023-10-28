7f4552dde1a439a5e7339de0461f92a1fa1c518c
Gab <gabriel.tronchoni@gmail.com>
2023-10-29 01:21:24 +0200

    user fix

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -10,7 +10,7 @@ alias renewarchroot="sudo podman kill --all && distrobox-rm -f --root arch && di
 alias renewarchnvidia="podman kill --all && distrobox-rm -f arch && distrobox-create --pull latest --name arch --nvidia --image ghcr.io/elgabo86/archgab:latest && distrobox enter arch"
 alias renewarchrootnvidia="sudo podman kill --all && distrobox-rm -f --root arch && distrobox-create --pull latest --name arch --nvidia --image ghcr.io/elgabo86/archgab:latest --root && distrobox enter --root arch"
 
-alias flatpak-fix-overrides="sudo flatpak override --filesystem=xdg-config/gtk-3.0:ro && sudo flatpak override --filesystem=xdg-config/MangoHud:ro && sudo flatpak override --filesystem=xdg-config/gtk-4.0:ro && sudo flatpak override --env=OBS_VKCAPTURE=1"
+alias flatpak-fix-overrides="flatpak override --user --filesystem=xdg-config/gtk-3.0:ro && flatpak override --user --filesystem=xdg-config/MangoHud:ro && flatpak override --user --filesystem=xdg-config/gtk-4.0:ro && flatpak override --user --env=OBS_VKCAPTURE=1"
 
 # Ignore duplicate
 HISTCONTROL=ignoredups
