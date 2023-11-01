dd11e33aa8330e951d44c91b40de52b660c8fb6c
Gab <gabriel.tronchoni@gmail.com>
2023-11-01 22:12:00 +0100

    polish

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -1,10 +1,8 @@
 # Personnal alias
 
-alias goarch="distrobox enter arch"
+alias goarch="distrobox enter archgab"
 
-alias goarch-root="distrobox enter --root arch"
-
-alias gablue-tailscale-init="sudo firewall-cmd --add-masquerade --zone=FedoraWorkstation --permanent && sudo firewall-cmd --add-interface=tailscale0 --zone=trusted --permanent && sudo tailscale up --login-server https://headscale.gabserv.duckdns.org"
+alias goarch-root="distrobox enter --root archgab"
 
 # Ignore duplicate
 HISTCONTROL=ignoredups
