028a7f0da6b8dbe8f8ff071fe274df90162de531
Gab <gabriel.tronchoni@gmail.com>
2023-12-02 13:43:24 +0100

    fix 2

--- config/scripts/fixbluez.sh
+++ config/scripts/fixbluez.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree override replace https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-5.68-2.fc39.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-libs-5.68-2.fc39.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-cups-5.68-2.fc39.x86_64.rpm
+rpm-ostree override replace https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-5.68-2.fc39.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-libs-5.68-2.fc39.x86_64.rpm https://kojipkgs.fedoraproject.org//packages/bluez/5.68/2.fc39/x86_64/bluez-cups-5.68-2.fc39.x86_64.rpm https://kojipkgs.fedoraproject.org/packages/bluez/5.68/2.fc39/x86_64/bluez-obexd-5.68-2.fc39.x86_64.rpm
