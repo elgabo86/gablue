276b1b1fdf1f25f1a7ea09cc550733880e65cfc2
elgabo86 <gabriel.tronchoni@gmail.com>
2023-12-27 15:17:06 +0100

    fix best vid script

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -3,8 +3,8 @@
 alias goarch="distrobox enter archgab"
 
 alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --embed-thumbnail --embed-metadata --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s'"
-alias dlv-mp4="yt-dlp --format best --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
-alias dlv-mkv="yt-dlp --format best --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
+alias dlv-mp4="yt-dlp --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
+alias dlv-mkv="yt-dlp --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 
 # Ignore duplicate
 HISTCONTROL=ignoredups
