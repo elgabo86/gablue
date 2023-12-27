c36bdbdca9bcacaf90de5f4fc82a77470dbe27c1
elgabo86 <gabriel.tronchoni@gmail.com>
2023-12-27 15:38:10 +0100

    fix audio quality

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -2,7 +2,7 @@
 
 alias goarch="distrobox enter archgab"
 
-alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --embed-thumbnail --embed-metadata --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s'"
+alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --audio-quality 0 --embed-thumbnail --embed-metadata --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s'"
 alias dlv-mp4="yt-dlp --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 alias dlv-mkv="yt-dlp --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 
