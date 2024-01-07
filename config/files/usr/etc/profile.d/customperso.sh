4a8db4bfd3e4717be8251533b415ccd0c99ec957
elgabo86 <gabriel.tronchoni@gmail.com>
2024-01-07 02:59:00 +0100

    add miniserve podman command

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -6,6 +6,8 @@ alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --audio-quality 0
 alias dlv-mp4="yt-dlp --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 alias dlv-mkv="yt-dlp --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 
+alias miniserve="podman run -v ./:/share:Z -p 8080:8080 --rm --rmi -it docker.io/svenstaro/miniserve /share"
+
 # Ignore duplicate
 HISTCONTROL=ignoredups
 
