21e77b4b19dfeed3967b2ea07c0b0bb102ad5af5
elgabo86 <gabriel.tronchoni@gmail.com>
2024-01-07 15:45:16 +0100

    fix miniserve ip host

--- config/files/usr/etc/profile.d/customperso.sh
+++ config/files/usr/etc/profile.d/customperso.sh
@@ -6,7 +6,7 @@ alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --audio-quality 0
 alias dlv-mp4="yt-dlp --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 alias dlv-mkv="yt-dlp --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 
-alias miniserve="podman run -v ./:/share:Z -p 8080:8080 --rm --rmi -it docker.io/svenstaro/miniserve /share"
+alias miniserve="podman run -v ./:/share:Z --network host --rm --rmi -it docker.io/svenstaro/miniserve /share"
 
 alias ffsend="podman run --rm --rmi -it -v $(pwd):/data:Z docker.io/timvisee/ffsend:latest upload -y"
 alias ffsendp="podman run --rm --rmi -it -v $(pwd):/data:Z docker.io/timvisee/ffsend:latest upload --password -y"
