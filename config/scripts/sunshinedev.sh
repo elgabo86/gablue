93eb2682d00455b2cb5d75d20d121bd111e8a05d
elgabo86 <gabriel.tronchoni@gmail.com>
2024-03-14 21:34:23 +0100

    update sunshine

--- config/scripts/sunshinedev.sh
+++ config/scripts/sunshinedev.sh
@@ -3,4 +3,4 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-rpm-ostree install https://github.com/LizardByte/Sunshine/releases/download/v0.22.0/sunshine-fedora-39-amd64.rpm
+rpm-ostree install https://github.com/LizardByte/Sunshine/releases/download/v0.22.1/sunshine-fedora-39-amd64.rpm
