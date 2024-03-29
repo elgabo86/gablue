316bc7a1bbe6753f7ac0f21ef991ef7a6338e5e1
elgabo86 <gabriel.tronchoni@gmail.com>
2024-03-29 12:14:30 +0100

    test to readd nvidia patch

--- config/scripts/nvidiasyncpatchbeta.sh
+++ config/scripts/nvidiasyncpatchbeta.sh
@@ -4,7 +4,6 @@
 set -oue pipefail
 
 # Install Explicit Sync Patches on Nvidia builds
-wget https://copr.fedorainfracloud.org/coprs/gloriouseggroll/nvidia-explicit-sync/repo/fedora-39/gloriouseggroll-nvidia-explicit-sync-fedora-39.repo?arch=x86_64 -O /etc/yum.repos.d/_copr_gloriouseggroll-nvidia-explicit-sync.repo
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:gloriouseggroll:nvidia-explicit-sync xorg-x11-server-Xwayland
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:gloriouseggroll:nvidia-explicit-sync egl-wayland
-rm /etc/yum.repos.d/_copr_gloriouseggroll-nvidia-explicit-sync.repo
+rpm-ostree override replace --experimental https://download.copr.fedorainfracloud.org/results/gloriouseggroll/nvidia-explicit-sync/fedora-39-x86_64/07116611-xorg-x11-server-Xwayland/xorg-x11-server-Xwayland-23.2.4-5.20240307git64341c4.fc39.x86_64.rpm
+rpm-ostree override replace --experimental https://download.copr.fedorainfracloud.org/results/gloriouseggroll/nvidia-explicit-sync/fedora-39-x86_64/07116519-egl-wayland/egl-wayland-1.1.13-2.fc39.x86_64.rpm
+
