7524e5994a13e9bbd7ac55e5b205f0a455245433
Gab <gabriel.tronchoni@gmail.com>
2024-04-08 15:46:54 +0200

    upstream is now good, test for user.conf modif

--- config/scripts/customsystemd.sh
+++ config/scripts/customsystemd.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-
+cp /usr/etc/systemd/user.conf /etc/systemd/user.conf
 sed -i 's/#DefaultLimitNOFILE=/DefaultLimitNOFILE=4096:524288/' /etc/systemd/user.conf && \
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/user.conf && \
 sed -i 's/#DefaultTimeoutStopSec.*/DefaultTimeoutStopSec=15s/' /etc/systemd/system.conf
