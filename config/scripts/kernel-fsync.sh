b0c54c69f0a543d532aa8931d3eb960fd5260bcf
elgabo86 <gabriel.tronchoni@gmail.com>
2024-04-23 23:51:15 +0200

    fix 1

--- config/scripts/kernel-fsync.sh
+++ config/scripts/kernel-fsync.sh
@@ -7,6 +7,6 @@ set -oue pipefail
 
 # Install kernel-fsync
 wget https://copr.fedorainfracloud.org/coprs/sentry/kernel-fsync/repo/fedora-39/sentry-kernel-fsync-fedora-39.repo -O /etc/yum.repos.d/kernel-fsync.repo
-rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:sentry:kernel-fsync' kernel kernel-core kernel-modules kernel-modules-core kernel-modules-extra kernel-uki-virt
 rm /etc/yum.repos.d/kernel-fsync.repo
 
