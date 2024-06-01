7db61e8e1621b8a18b0337da690e1579cc19085a
405C <gabriel.tronchoni@gmail.com>
2024-06-01 17:39:38 +0200

    remove update script, bad

--- config/scripts/updateopenrgbrules.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -ouex pipefail
-
-wget -O /usr/lib/udev/rules.d/60-openrgb.rules https://gitlab.com/CalcProgrammer1/OpenRGB/-/jobs/artifacts/master/raw/60-openrgb.rules?job=Linux+64+AppImage&inline=false
