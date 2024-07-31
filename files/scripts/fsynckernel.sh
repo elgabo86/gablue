e1e1facd4fe3cb819407782b64fe93dfe078c4f4
elgabo86 <gabriel.tronchoni@gmail.com>
2024-07-31 17:01:03 +0200

    add fsync kernel patch

---
+++ files/scripts/fsynckernel.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+rpm-ostree cliwrap install-to-root / && \
+
+
+rpm-ostree override replace --experimental \
+        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-core-*.rpm \
+        /tmp/kernel-rpms/kernel-modules-*.rpm
---
+++ files/scripts/fsynckernelnvidia.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+rpm-ostree cliwrap install-to-root / && \
+
+
+rpm-ostree override replace --experimental \
+        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+        /tmp/kernel-rpms/kernel-core-*.rpm \
+        /tmp/kernel-rpms/kernel-modules-*.rpm
+
+curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh && \
+chmod +x /tmp/nvidia-install.sh && \
+IMAGE_NAME="kinoite" RPMFUSION_MIRROR="" FEDORA_MAJOR_VERSION="40" /tmp/nvidia-install.sh
+rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
