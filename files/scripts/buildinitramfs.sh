625cf07b4c01ff8408d5bd5e04b3de9e2d1c4860
405C <gabriel.tronchoni@gmail.com>
2024-08-10 22:27:17 +0200

    add initramfs build script

---
+++ files/scripts/buildinitramfs.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+KERNEL_SUFFIX=""
+QUALIFIED_KERNEL="$(rpm -qa | grep -P 'kernel-(|'"$KERNEL_SUFFIX"'-)(\d+\.\d+\.\d+)' | sed -E 's/kernel-(|'"$KERNEL_SUFFIX"'-)//')"
+/usr/libexec/rpm-ostree/wrapped/dracut --no-hostonly --kver "$QUALIFIED_KERNEL" --reproducible -v --add ostree -f "/lib/modules/$QUALIFIED_KERNEL/initramfs.img"
+
+chmod 0600 /lib/modules/$QUALIFIED_KERNEL/initramfs.img
