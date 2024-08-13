a5aec0dd1d0739ba8539c3a68bef61e328eac31b
Gab <gabriel.tronchoni@gmail.com>
2024-08-13 14:25:38 +0200

    update some files from bluefin
    change logo os
    
    revert move bling module

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -6,7 +6,3 @@ set -ouex pipefail
 IMAGE_DATE=$(date +%Y%m%d.%H)
 MAJOR_RELEASE_VERSION=$(grep -oP '[0-9]*' /etc/fedora-release)
 sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Gablue ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE}\"," /usr/lib/os-release
-
-
-# Remove ublue update notification
-sed -i 's/dbus_notify = true/dbus_notify = false/g' /etc/ublue-update/ublue-update.toml
