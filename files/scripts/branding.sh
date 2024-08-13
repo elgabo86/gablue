c142515a878aa19f4b86527bab8f82e240fb1356
Gab <gabriel.tronchoni@gmail.com>
2024-08-13 10:32:53 +0200

    redo branding, hide update notification, ajust ujust commands and gablue-help

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -3,4 +3,10 @@
 set -ouex pipefail
 
 # Branding for Gablue
-sed -i '/^PRETTY_NAME/s/Kinoite/Gablue/' /usr/lib/os-release
+IMAGE_DATE=$(date +%Y%m%d.%H)
+MAJOR_RELEASE_VERSION=$(grep -oP '[0-9]*' /etc/fedora-release)
+sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Gablue ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE}\"," /usr/lib/os-release
+
+
+# Remove ublue update notification
+sed -i 's/dbus_notify = true/dbus_notify = false/g' /etc/ublue-update/ublue-update.toml
