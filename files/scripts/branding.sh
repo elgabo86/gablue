2d0555f346c69b3cd903e1404d94344579d5b5e2
Gab <gabriel.tronchoni@gmail.com>
2024-08-13 15:59:22 +0200

    add nofile limitation from bluefin add script on boot to apply commands on each boot remove custom icons for the moment (bad) remove an old script

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -6,3 +6,6 @@ set -ouex pipefail
 IMAGE_DATE=$(date +%Y%m%d.%H)
 MAJOR_RELEASE_VERSION=$(grep -oP '[0-9]*' /etc/fedora-release)
 sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Gablue ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE}\"," /usr/lib/os-release
+
+
+#ln -sf ../places/distributor-logo.svg /usr/share/icons/hicolor/scalable/apps/start-here.svg
