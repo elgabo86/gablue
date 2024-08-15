325449aaa4db2e1a5cb77f60899d09f7933ce27f
405C <gabriel.tronchoni@gmail.com>
2024-08-15 16:25:38 +0200

    fix icon start menu

--- files/scripts/branding.sh
+++ files/scripts/branding.sh
@@ -7,5 +7,7 @@ IMAGE_DATE=$(date +%Y%m%d.%H)
 MAJOR_RELEASE_VERSION=$(grep -oP '[0-9]*' /etc/fedora-release)
 sed -i "s,^PRETTY_NAME=.*,PRETTY_NAME=\"Gablue ${MAJOR_RELEASE_VERSION}.${IMAGE_DATE}\"," /usr/lib/os-release
 
-
+ln -sf ../../../hicolor/scalable/places/start-here.svg /usr/share/icons/Papirus/16x16/panel/start-here.svg
+ln -sf ../../../hicolor/scalable/places/start-here.svg /usr/share/icons/Papirus/22x22/panel/start-here.svg
+ln -sf ../../../hicolor/scalable/places/start-here.svg /usr/share/icons/Papirus/24x24/panel/start-here.svg
 ln -sf ../../../hicolor/scalable/places/start-here.svg /usr/share/icons/Papirus/symbolic/places/start-here-symbolic.svg
