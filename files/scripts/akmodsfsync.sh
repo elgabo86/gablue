ea34864fd30bb3df3f3b9ad7f595a1f70e9a50e2
elgabo86 <gabriel.tronchoni@gmail.com>
2024-08-20 22:45:01 +0200

    move evdi module to extra remove fuse because is already on the main image

--- files/scripts/akmodsfsync.sh
+++ files/scripts/akmodsfsync.sh
@@ -12,8 +12,9 @@ rpm-ostree install \
     /tmp/akmods-rpms/kmods/*openrazer*.rpm \
     /tmp/akmods-rpms/kmods/*v4l2loopback*.rpm \
     /tmp/akmods-rpms/kmods/*wl*.rpm \
-    /tmp/akmods-rpms/kmods/*evdi*.rpm \
+    /tmp/akmods-extra-rpms/kmods/*evdi*.rpm \
     /tmp/akmods-extra-rpms/kmods/*nct6687*.rpm \
+    /tmp/akmods-extra-rpms/kmods/*gcadapter_oc*.rpm \
     /tmp/akmods-extra-rpms/kmods/*bmi260*.rpm && \
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 
