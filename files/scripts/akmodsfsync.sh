399fb9fe1213dbd19d13be51c2ffcb5ce271eb0e
Gab <gabriel.tronchoni@gmail.com>
2024-08-20 11:19:02 +0200

    reorder and ajust modules add cleanup script

---
+++ files/scripts/akmodsfsync.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Tell this script to exit if there are any errors.
+# You should have this in every custom script, to ensure that your completed
+# builds actually ran successfully without any errors!
+set -oue pipefail
+
+sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo && \
+curl -Lo /etc/yum.repos.d/negativo17-fedora-multimedia.repo https://negativo17.org/repos/fedora-multimedia.repo && \
+rpm-ostree install \
+    /tmp/akmods-rpms/kmods/*xone*.rpm \
+    /tmp/akmods-rpms/kmods/*openrazer*.rpm \
+    /tmp/akmods-rpms/kmods/*v4l2loopback*.rpm \
+    /tmp/akmods-rpms/kmods/*wl*.rpm \
+    /tmp/akmods-extra-rpms/kmods/*nct6687*.rpm \
+    /tmp/akmods-extra-rpms/kmods/*bmi260*.rpm && \
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
--- files/scripts/nvidiadrivers.sh
+++ files/scripts/nvidiadrivers.sh
@@ -5,13 +5,6 @@
 # builds actually ran successfully without any errors!
 set -oue pipefail
 
-rpm-ostree cliwrap install-to-root / && \
-
-rpm-ostree override replace --experimental \
-        /tmp/kernel-rpms/kernel-[0-9]*.rpm \
-        /tmp/kernel-rpms/kernel-core-*.rpm \
-        /tmp/kernel-rpms/kernel-modules-*.rpm
-
 curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh && \
 chmod +x /tmp/nvidia-install.sh && \
 IMAGE_NAME="kinoite" FEDORA_MAJOR_VERSION="40" /tmp/nvidia-install.sh
