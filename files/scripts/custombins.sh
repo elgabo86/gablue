6aaade9949562cb6a6829f9e2c19edb9709731fb
Gab <gabriel.tronchoni@gmail.com>
2024-08-26 07:53:56 +0200

    remove bluebuild implementation and add custom build containerfile to stable images fix nvidia copy files

--- files/scripts/custombins.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Tell build process to exit if there are any errors.
-set -ouex pipefail
-
-wget https://github.com/aandrew-me/tgpt/releases/latest/download/tgpt-linux-amd64 -O /usr/bin/tgpt
-chmod +x /usr/bin/tgpt
