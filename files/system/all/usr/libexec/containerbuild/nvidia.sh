28d3f37ac6513bd691f767391ca494392c94d5ec
elgabo86 <gabriel.tronchoni@gmail.com>
2024-09-01 21:30:58 +0200

    change variable

--- files/system/all/usr/libexec/containerbuild/nvidia.sh
+++ files/system/all/usr/libexec/containerbuild/nvidia.sh
@@ -7,5 +7,5 @@ set -ouex pipefail
 
 curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh && \
 chmod +x /tmp/nvidia-install.sh && \
-IMAGE_NAME="kinoite" FEDORA_MAJOR_VERSION="40" /tmp/nvidia-install.sh
+IMAGE_NAME="${SOURCE_IMAGE}" FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}" /tmp/nvidia-install.sh
 rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
