cd369b1df9bc5328b36c757a9d724d62fe2d5d98
elgabo86 <gabriel.tronchoni@gmail.com>
2024-09-04 22:55:49 +0200

    add ublue-os-staging repo

--- files/system/all/usr/libexec/containerbuild/copr.sh
+++ files/system/all/usr/libexec/containerbuild/copr.sh
@@ -6,6 +6,7 @@ curl -Lo /usr/bin/copr https://raw.githubusercontent.com/ublue-os/COPR-command/m
 chmod +x /usr/bin/copr && \
 curl -Lo /etc/yum.repos.d/_copr_che-nerd-fonts.repo https://copr.fedorainfracloud.org/coprs/che/nerd-fonts/repo/fedora-"${FEDORA_MAJOR_VERSION}"/che-nerd-fonts-fedora-"${FEDORA_MAJOR_VERSION}".repo
 curl -Lo /etc/yum.repos.d/_copr_kylegospo-bazzite.repo https://copr.fedorainfracloud.org/coprs/kylegospo/bazzite/repo/fedora-"${FEDORA_MAJOR_VERSION}"/kylegospo-bazzite-fedora-"${FEDORA_MAJOR_VERSION}".repo && \
+curl -Lo /etc/yum.repos.d/ublue-os-staging-fedora.repo https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-"${FEDORA_MAJOR_VERSION}"/ublue-os-staging-fedora-"${FEDORA_MAJOR_VERSION}".repo && \
 curl -Lo /etc/yum.repos.d/_copr_matte-schwartz-sunshine.repo https://copr.fedorainfracloud.org/coprs/matte-schwartz/sunshine/repo/fedora-"${FEDORA_MAJOR_VERSION}"/matte-schwartz-sunshine-fedora-"${FEDORA_MAJOR_VERSION}".repo && \
 curl -Lo /etc/yum.repos.d/_copr_sramanujam-atuin.repo https://copr.fedorainfracloud.org/coprs/sramanujam/atuin/repo/fedora-"${FEDORA_MAJOR_VERSION}"/sramanujam-atuin-fedora-"${FEDORA_MAJOR_VERSION}".repo  && \
 curl -Lo /etc/yum.repos.d/_copr_luisbocanegra-kde-material-you-colors.repo https://copr.fedorainfracloud.org/coprs/luisbocanegra/kde-material-you-colors/repo/fedora-"${FEDORA_MAJOR_VERSION}"/luisbocanegra-kde-material-you-colors-fedora-"${FEDORA_MAJOR_VERSION}".repo && \
