7bbed1043fd0b0475c4cfe62ab6afee18c523270
405C <gabriel.tronchoni@gmail.com>
2024-09-05 21:22:05 +0200

    try to fix nvidia build

--- files/system/all/usr/libexec/containerbuild/nvidia.sh
+++ files/system/all/usr/libexec/containerbuild/nvidia.sh
@@ -5,7 +5,13 @@
 # builds actually ran successfully without any errors!
 set -ouex pipefail
 
+rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:kylegospo:bazzite-multilib \
+        mesa-libEGL.i686
+
 curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh && \
 chmod +x /tmp/nvidia-install.sh && \
 IMAGE_NAME="${SOURCE_IMAGE}" FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}" /tmp/nvidia-install.sh
 rm -f /usr/share/vulkan/icd.d/nouveau_icd.*.json
+ln -s libnvidia-ml.so.1 /usr/lib64/libnvidia-ml.so
