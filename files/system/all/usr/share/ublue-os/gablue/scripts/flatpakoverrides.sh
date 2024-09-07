d6dcd4ad6cb79a8e5322faa58c683f5800d9e5fd
elgabo86 <gabriel.tronchoni@gmail.com>
2024-09-07 12:34:31 +0200

    add gamepad support in firefox add /media acces for bottles

--- files/system/all/usr/share/ublue-os/gablue/scripts/flatpakoverrides.sh
+++ files/system/all/usr/share/ublue-os/gablue/scripts/flatpakoverrides.sh
@@ -6,7 +6,9 @@ flatpak override --user --filesystem=xdg-config/MangoHud:ro
 flatpak override --user --filesystem=xdg-config/gtk-4.0:ro
 flatpak override --user --env=OBS_VKCAPTURE=1
 flatpak override --user --filesystem=/run/media
+flatpak override --user --filesystem=/media
 flatpak override --user --filesystem=xdg-download
 flatpak override com.usebottles.bottles --user --filesystem=xdg-data/applications
 flatpak override com.usebottles.bottles --user --env=DXVK_ASYNC=1
 flatpak override com.usebottles.bottles --user --env=DXVK_GPLASYNCCACHE=1
+flatpak override org.mozilla.firefox --user --filesystem=/run/udev:ro
