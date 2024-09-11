f4b6b2639c77b0d92d465f403739ee34e400729b
elgabo86 <gabriel.tronchoni@gmail.com>
2024-09-11 22:35:43 +0200

    remove old nvidia fix

--- files/system/all/usr/libexec/containerbuild/nvidia.sh
+++ files/system/all/usr/libexec/containerbuild/nvidia.sh
@@ -5,10 +5,6 @@
 # builds actually ran successfully without any errors!
 set -ouex pipefail
 
-rpm-ostree install \
-        mesa-libEGL.i686 \
-        expat.i686
-
 curl -Lo /tmp/nvidia-install.sh https://raw.githubusercontent.com/ublue-os/hwe/main/nvidia-install.sh && \
 chmod +x /tmp/nvidia-install.sh && \
 IMAGE_NAME="${SOURCE_IMAGE}" FEDORA_MAJOR_VERSION="${FEDORA_MAJOR_VERSION}" /tmp/nvidia-install.sh
