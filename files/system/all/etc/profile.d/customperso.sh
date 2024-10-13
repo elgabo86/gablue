5e73ac2a45ea48e44699d0f3e88a400257b880ba
Gab <gab@gen1.home>
2024-10-14 00:02:25 +0200

    update scripts

--- files/system/all/etc/profile.d/customperso.sh
+++ files/system/all/etc/profile.d/customperso.sh
@@ -8,7 +8,7 @@ alias dlv-mp3="yt-dlp -x --format bestaudio --audio-format mp3 --audio-quality 0
 alias dlv-mp4="yt-dlp --recode-video mp4 --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 alias dlv-mkv="yt-dlp --recode-video mkv --paths $(xdg-user-dir DOWNLOAD) -o '%(title)s.%(ext)s' --embed-subs --sub-langs all --embed-thumbnail"
 
-alias miniserve="podman run -v ./:/share:Z --network host --rm --rmi -it docker.io/svenstaro/miniserve /share -r"
+alias miniserve="podman kill miniserve; sleep 2; podman run -v ./:/share:Z --network host --name miniserve --rm --rmi -it docker.io/svenstaro/miniserve /share -r"
 
 alias ffsend="podman run --rm --rmi -it -v $(pwd):/data:Z docker.io/timvisee/ffsend:latest upload -y"
 alias ffsendp="podman run --rm --rmi -it -v $(pwd):/data:Z docker.io/timvisee/ffsend:latest upload --password -y"
