91731d0595060bdf87d001c8a21945a558e56b73
Gab <gabriel.tronchoni@gmail.com>
2024-10-19 01:22:58 +0200

    add windows fix gamepad launcher  + shortcut creation

---
+++ files/system/all/usr/share/ublue-os/gablue/scripts/createwinshortcutfix.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+fullpath="$1"
+
+onlypath=$(dirname "$fullpath")
+
+onlyapp=$(basename "$fullpath" .exe)
+
+echo "#!/bin/bash" > "$onlypath"/"$onlyapp".sh
+echo 'flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 0 -t "REG_DWORD" &&'   >> "$onlypath"/"$onlyapp".sh
+echo "/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable '$fullpath' ;" >> "$onlypath"/"$onlyapp".sh
+echo 'flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 1 -t "REG_DWORD"'  >> "$onlypath"/"$onlyapp".sh
+
+chmod +x "$onlypath"/"$onlyapp".sh
