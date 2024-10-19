835bbb666f3a0fc7394fcd71fbf695ae967e4a82
Gab <gabriel.tronchoni@gmail.com>
2024-10-19 14:27:42 +0200

    change fsync variable to kernel try 6.11.4-201 again

---
+++ files/system/all/usr/libexec/containerbuild/kernel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+rpm-ostree cliwrap install-to-root /
+
+rpm-ostree override replace --experimental \
+    /tmp/kernel-rpms/kernel-[0-9]*.rpm \
+    /tmp/kernel-rpms/kernel-core-*.rpm \
+    /tmp/kernel-rpms/kernel-modules-*.rpm
