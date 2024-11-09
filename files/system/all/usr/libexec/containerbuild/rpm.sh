a320c860354377005f4f235d4f85e32d8b00558d
Gab <gabriel.tronchoni@gmail.com>
2024-11-09 10:41:00 +0100

    Revert "add wallpaper engine"
    This reverts commit e7b7217266c9c172ad6cb951007ea288ca95834b.

--- files/system/all/usr/libexec/containerbuild/rpm.sh
+++ files/system/all/usr/libexec/containerbuild/rpm.sh
@@ -75,8 +75,7 @@ if [ "$SOURCE_IMAGE" == "kinoite" ]; then
     libadwaita-qt5 \
     libadwaita-qt6 \
     kde-runtime-docs \
-    kdeplasma-addons \
-    wallpaper-engine-kde-plugin
+    kdeplasma-addons
 fi
 
 #Fonts
