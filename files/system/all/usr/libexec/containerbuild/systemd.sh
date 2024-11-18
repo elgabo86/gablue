3b89a3166a2e647e343b49bb3c5c2b7d42620c46
Gab <gabriel.tronchoni@gmail.com>
2024-11-18 16:39:47 +0100

    remove "enable tailscaled"

--- files/system/all/usr/libexec/containerbuild/systemd.sh
+++ files/system/all/usr/libexec/containerbuild/systemd.sh
@@ -5,7 +5,6 @@ set -eoux pipefail
 
 systemctl enable -f system-flatpak-setup.service
 systemctl enable -f sunshine-workaround.service
-systemctl enable -f tailscaled.service
 systemctl enable -f gamescope-workaround.service
 systemctl enable -f earlyoom.service
 
