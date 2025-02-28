651f71bec695f6b670cd28a79a9cf51a1034c4c4
Gab <gabriel.tronchoni@gmail.com>
2025-02-28 18:36:17 +0100

    update wine launchers and shortcut creation scripts

--- files/system/all/usr/share/ublue-os/gablue/scripts/launchwin.sh
+++ files/system/all/usr/share/ublue-os/gablue/scripts/launchwin.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 fullpath="$1"
-flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 1 -t "REG_DWORD" &&
-sleep 0.5 &&
+waitwine flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 1 -t "REG_DWORD" &&
 /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable "$fullpath"
