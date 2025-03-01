9e984d81d8ef557c3364e0d9a2de761508319820
Gab <gabriel.tronchoni@gmail.com>
2025-03-01 12:11:03 +0100

    fix windows launcher

--- files/system/all/usr/share/ublue-os/gablue/scripts/createwinshortcutfix.sh
+++ files/system/all/usr/share/ublue-os/gablue/scripts/createwinshortcutfix.sh
@@ -7,7 +7,9 @@ onlypath=$(dirname "$fullpath")
 onlyapp=$(basename "$fullpath" .exe)
 
 echo "#!/bin/bash" > "$onlypath"/"$onlyapp".sh
-echo 'waitwine flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 0 -t "REG_DWORD" &&'   >> "$onlypath"/"$onlyapp".sh
+echo 'if ! pgrep -x "wineserver" > /dev/null; then' >> "$onlypath"/"$onlyapp".sh
+echo '    waitwine flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 0 -t "REG_DWORD" &&'   >> "$onlypath"/"$onlyapp".sh
+echo 'fi' >> "$onlypath"/"$onlyapp".sh
 echo "/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable '$fullpath' ;" >> "$onlypath"/"$onlyapp".sh
 echo 'flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 1 -t "REG_DWORD"'  >> "$onlypath"/"$onlyapp".sh
 
--- files/system/all/usr/share/ublue-os/gablue/scripts/launchwin.sh
+++ files/system/all/usr/share/ublue-os/gablue/scripts/launchwin.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 fullpath="$1"
-waitwine flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 1 -t "REG_DWORD" &&
+
+if ! pgrep -x "wineserver" > /dev/null; then
+    waitwine flatpak run --command=bash com.usebottles.bottles -c "bottles-cli reg add -b def -k 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus' -v 'DisableHidraw' -d 1 -t 'REG_DWORD'"
+fi
+
 /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable "$fullpath"
