96d25f2060b1c46f92e02d17635c7253893213de
Gab <gabriel.tronchoni@gmail.com>
2025-03-06 19:16:55 +0100

    fix windows gamepad fix launcher

--- files/system/all/usr/share/ublue-os/gablue/scripts/launchwinfix.sh
+++ files/system/all/usr/share/ublue-os/gablue/scripts/launchwinfix.sh
@@ -2,7 +2,7 @@
 fullpath="$1"
 
 if ! pgrep -x "wineserver" > /dev/null; then
-    waitwine flatpak run --command=bash com.usebottles.bottles -c "bottles-cli reg add -b def -k 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus' -v 'DisableHidraw' -d 1 -t 'REG_DWORD'"
+    waitwine flatpak run --command=bottles-cli com.usebottles.bottles reg add -b def -k "HKEY_LOCAL_MACHINE\System\CurrentControlSet\Services\winebus" -v "DisableHidraw" -d 0 -t "REG_DWORD"
 fi
 
 /usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable "$fullpath" ;
