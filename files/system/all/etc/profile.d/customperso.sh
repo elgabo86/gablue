cf457f961b9dcd0c0d4dbffaaa837093a7cf05cf
Gab <gabriel.tronchoni@gmail.com>
2025-05-12 00:16:14 +0200

    update customperso.sh [build]

--- files/system/all/etc/profile.d/customperso.sh
+++ files/system/all/etc/profile.d/customperso.sh
@@ -19,14 +19,6 @@ alias gogpt-web="tgpt --provider blackboxai -i"
 
 alias gogpt-pro="tgpt --provider phind -i"
 
-function genimg() {
-       tgpt -img "$*"
-}
-
-function ask() {
-       tgpt --provider duckduckgo "$*"
-}
-
 alias wkill="pgrep -i '(.*\\.(exe|dll|msi|bat|com|lnk)$)|(.*(wine|proton).*)' | xargs -n1 kill -9 ; pgrep -f bottles | xargs -I {} sh -c 'ps -p {} -o comm= | grep -q \"^bwrap$\" && kill -9 {}'"
 
 alias wrun="/usr/bin/flatpak run --branch=stable --arch=x86_64 --command=bottles-cli --file-forwarding com.usebottles.bottles run --bottle def --executable"
