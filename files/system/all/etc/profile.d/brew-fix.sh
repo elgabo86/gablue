9bce08565e073f657ddf545f8bcb46ba7aa18a10
Gab <gabriel.tronchoni@gmail.com>
2025-10-06 13:09:53 +0200

    remove brew-fix, fix qwen.desktop instead [build]

--- files/system/all/etc/profile.d/brew-fix.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Script pour charger l'environnement brew dans les shells non interactifs
-if [ -d "/home/linuxbrew/.linuxbrew" ]; then
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-fi
